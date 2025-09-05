d772a8054fc76d15571f9a58c3849bab35826d56
phuvt-vn <ldcc.thanhphu@lotte.vn>
2025-09-05 15:17:53 +0700

    Integrate Kafka modules with Microservice

--- udemy/Event-Driven-Microservices-Spring-Boot,-Kafka-and-Elastic/microservices-demo/twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/init/impl/KafkaStreamInitializer.java
+++ udemy/Event-Driven-Microservices-Spring-Boot,-Kafka-and-Elastic/microservices-demo/twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/init/impl/KafkaStreamInitializer.java
@@ -24,6 +24,7 @@ public class KafkaStreamInitializer implements StreamInitializer {
     @Override
     public void init() {
         kafkaAdminClient.createTopics();
+        kafkaAdminClient.checkSchemaRegistry();
         LOG.info("Topics with name {} is ready for operations!", kafkaConfigData.getTopicNamesToCreate().toArray());
     }
 }
